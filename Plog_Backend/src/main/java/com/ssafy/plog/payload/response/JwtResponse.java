f68a52fe011941d84f2364b5adafcdc065452953
신지식 <s48011@naver.com>
2020-08-06 11:53:50 +0900

    [ADD] Login - JWT & Security

---
+++ Plog_Backend/src/main/java/com/ssafy/plog/payload/response/JwtResponse.java
@@ -0,0 +1,90 @@
+package com.ssafy.plog.payload.response;
+
+import java.util.List;
+
+public class JwtResponse {
+	private String token;
+	private String type = "Bearer";
+	private Long id;
+	private String username;
+	private String email;
+	private List<String> roles;
+
+	//추가 
+	private String birthday;
+	private String phone;
+	
+	
+	public JwtResponse(String accessToken, Long id, String username, String email, String birthday, String phone, List<String> roles) {
+		this.token = accessToken;
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		
+		this.phone = phone;
+		this.birthday = birthday;
+		
+		
+		this.roles = roles;
+	}
+
+	public String getAccessToken() {
+		return token;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.token = accessToken;
+	}
+
+	public String getTokenType() {
+		return type;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.type = tokenType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+}
---
+++ Plog_Backend/src/main/java/com/ssafy/plog/security/WebSecurityConfig.java
@@ -0,0 +1,71 @@
+package com.ssafy.plog.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.ssafy.plog.security.jwt.AuthEntryPointJwt;
+import com.ssafy.plog.security.jwt.AuthTokenFilter;
+import com.ssafy.plog.service.UserDetailsServiceImpl;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+		// securedEnabled = true,
+		// jsr250Enabled = true,
+		prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	UserDetailsServiceImpl userDetailsService;
+
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
+
+	@Bean
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable()
+			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.authorizeRequests().antMatchers("/auth/**").permitAll()
+			.antMatchers("/schedule/**").permitAll()
+			.antMatchers("/hashtag/**").permitAll()
+			.antMatchers("/note/**").permitAll()
+			.antMatchers("/post/**").permitAll()
+			.antMatchers("**").permitAll()
+			.anyRequest().authenticated();
+	
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+}
