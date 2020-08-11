cf8b33d0c86b72406538d3c3ad9a76a839efd4cb
임혜민 <rheem-hm1304@naver.com>
2020-08-12 08:57:42 +0900

    [ADD] frontend - profile edit modal

--- plog_frontend/src/views/account/Profile.vue
+++ plog_frontend/src/views/account/Profile.vue
@@ -31,7 +31,45 @@
                             <v-divider></v-divider>
                         </v-card-text>
                         <v-card-actions class="d-flex justify-end pt-0">
-                            <v-btn color="blue" text small class="font-weight-bold">update</v-btn>
+                            <v-btn color="blue" text small class="font-weight-bold" @click.stop="dialog = true">update</v-btn>
+                            <!-- 수정모달 -->
+                            <v-dialog v-model="dialog" max-width="400">
+                                  <v-card>
+                                    <v-card-title class="headline">Edit Profile</v-card-title>
+                                    <v-card-text class="pb-0">
+                                      <v-col cols="12" class="py-0 mt-10">
+                                        <v-text-field
+                                          label="Username" :placeholder="U.username" v-model="U.username"
+                                          filled rounded dense></v-text-field>
+                                      </v-col>
+                                      <v-col cols="12" class="py-0">
+                                        <v-text-field
+                                          label="E-mail" :placeholder="U.useremail" v-model="U.useremail"
+                                          filled rounded dense></v-text-field>
+                                      </v-col>
+                                      <v-col cols="12" class="py-0">
+                                        <v-text-field
+                                          label="Birthday" :placeholder="U.userbirthday" v-model="U.userbirthday"
+                                          hint="YYYY-MM-DD 의 형식으로 입력해주세요"
+                                          persistent-hint
+                                          filled rounded dense></v-text-field>
+                                      </v-col>
+                                      <v-col cols="12" class="py-0">
+                                        <v-text-field
+                                          label="Phone" :placeholder="U.userphone" v-model="U.userphone"
+                                          hint="000-0000-0000 의 형식으로 입력해주세요"
+                                          persistent-hint                                          
+                                          filled rounded dense></v-text-field>
+                                      </v-col>
+                                    </v-card-text>
+
+                                    <v-card-actions class="pt-5">
+                                      <v-spacer></v-spacer>
+                                      <v-btn color="orange" text @click="sendData" >Edit</v-btn>
+                                      <v-btn color="grey" text @click="dialog = false" >Close</v-btn>
+                                    </v-card-actions>
+                                  </v-card>
+                                </v-dialog>
                         </v-card-actions>
                     </v-card>
                     <v-card class="mx-auto mt-5">
@@ -85,6 +123,9 @@
                             <v-expansion-panel-header>more...</v-expansion-panel-header>
                             <v-expansion-panel-content class="px-0">
                                 <v-row class="text-center">
+                                    <v-col cols="12" class="py-0">
+                                        <v-btn small color="orange" @click="logOut" block dark>Log out</v-btn>                                   
+                                    </v-col>
                                     <v-col cols="12">
                                         <v-btn small color="red" block dark>withdraw</v-btn>                                   
                                     </v-col>
@@ -355,6 +396,7 @@ export default {
     },
     data() {
       return {
+        dialog: false,
         benched: 0,
         mygroups: [],
         Notes : [],
@@ -363,6 +405,14 @@ export default {
         hashtags : [],
         panel : true,
         model: null,
+        // 프로필 수정
+        U: {
+          username: '',
+          userbirthday: '',
+          useremail: '',
+          userpassword: '',
+          userphone: '',
+        },
         // 먼슬리 통계
         chart_data_formonth: [
             {schedule: 7, note: 2, date: 1},
@@ -485,6 +535,13 @@ export default {
       }
     },
     created() {
+      // 수정페이지 관련
+      this.U.username = this.$store.state.auth.user.username
+      this.U.userbirthday = this.$store.state.auth.user.birthday
+      this.U.useremail= this.$store.state.auth.user.email
+      this.U.userpassword= this.$store.state.auth.user.password
+      this.U.userphone= this.$store.state.auth.user.phone
+      // 북마크
       http.get('/post/list/bookmark',{
         params : {
           uid : this.$store.state.auth.user.id,
@@ -584,6 +641,9 @@ export default {
                 this.bmToggle = 1;
                 this.selected.pBookmark = 1;
             }
+        },
+        sendData() {
+          // 여기서 검증한 뒤에 axios 쏘기
         },
      },
     computed: {
