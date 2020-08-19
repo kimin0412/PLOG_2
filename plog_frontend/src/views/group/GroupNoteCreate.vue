5ac58afac44259c46125020513b9cd658c99177a
이민재 <pmqa@naver.com>
2020-08-19 05:33:11 +0000

    Update GroupNoteCreate.vue

--- plog_frontend/src/views/group/GroupNoteCreate.vue
+++ plog_frontend/src/views/group/GroupNoteCreate.vue
@@ -560,7 +560,7 @@ export default {
         const Entities = require("html-entities").XmlEntities;
         const entities = new Entities();
         content = entities.encode(content);
-        var resContent = '';
+        var resContent = content;
 
         var numOfHashTag = this.model.length;
         this.hashtags = "";
